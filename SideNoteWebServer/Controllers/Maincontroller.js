a9e981c71e5c63e25ea0c403dee840e249b060c7
Jerum <hubbertj@gmail.com>
2014-05-07 22:45:45 -0700

    Moved API functions
    Removed API functions from MainController.js to a new Request.js obj

--- SideNoteWebServer/Controllers/Maincontroller.js
+++ SideNoteWebServer/Controllers/Maincontroller.js
@@ -12,91 +12,6 @@
 	
 //--------------------------------------------------------------------------
 
-var validator = require(GLOBAL.LIB + 'Validate');
-var decrypter = require(GLOBAL.LIB + 'Decrypter');
-
-// var decryptCredentials = function(post) {
-// 	// Step 1 - Get Post parameters
-// 	var post = req.body;
-
-// 	// Step 2 - Decrypt Post parameters
-// 	var apiKey = decrypter.decrypt(post['apiKey']);
-// 	var email = decrypter.decrypt(post['email']);
-// 	var password = decrypter.decrypt(post['password']);
-	
-// 	return [apiKey, email, password];
-// };
-
-// var validateCredentials = function(apiKey, email, password) {
-// 	// Step 1 - Decrypt Post parameters
-// 	var credentials = decryptCredentials(post);
-// 	var apiKey = credentials['apiKey'];
-// 	var email = credentials['email'];
-// 	var password = credentials['password'];
-	
-// 	// Step 2 - Validate API KEY
-// 	validator.validateApiKey(apiKey);
-// 	validator.isValidEmailString(email);
-// 	validator.isValidPasswordString(password);
-	
-// 	// Step 3 - Validate e-mail / password combination
-// 	if(validator.validateCredential(email, password)) {
-// 		// Everything went well
-// 		// Return the credentials
-// 		return [apiKey, email, password];
-// 	}
-// 	else {
-// 		// Couldn't validate
-// 		return null;
-// 	}
-// };
-
-// var registerUser = function(req, res) {
-//     if(req.query.api){
-//     	api = req.query.api;
-//     }
-
-// 	if(!checkAPI(api)){
-// 		throw new Error("BAD API KEY!");
-// 	   	return;
-// 	}
-
-// 	console.log('continue the call!');
-// };
-
-// var loginUser = function(req, res) {
-// 	var credentials = decryptCredentials(req.body);
-	
-// 	if(credentials) {
-// 		// The credentials checked out
-// 		if(!validator.validateApiKey(credentials['apiKey']) {
-// 			// Invalid Api Key
-// 		}
-		
-// 		if(!validator.validateEmail(credentials['email'])) {
-// 		}
-		
-// 		if(!validator.validatePassword(credentials['password']) {
-// 		}
-		
-		
-// 	}
-// };
-
-// var getNote = function(req, res) {
-// 	var credentials = validateCredentials(req.body);
-		
-// 	if(credentials) {
-// 		// The credentials checked out
-// 	}
-// };
-
-// var getBundle = function(req, res) {
-// };
-
-
-
-
 var addTestResponds = function(app){
 	app.get('/test', function(req, res) {
 		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -112,8 +27,7 @@ var checkAPI = function(apikey) {
 	 return modelAPI.validate();
 }; 
 
-//starts express obj
-var startExrepss = function() {
+var startExpress = function() {
 	var express = require('express');
 	var app = express();
 	app.listen(appConfig().restPort);
@@ -127,185 +41,24 @@ var startExrepss = function() {
 		return false;
 	}
 };
-
-var app = startExrepss();
+//starts express obj
+var app = startExpress();
 
 if(app){
+	var request = require(GLOBAL.LIB + 'Request');
 
-	app.get('/:mod/:type/:id', function(req, res) {
-
-		//required parameters
-		var apikey = null; 
-		var call = {
-							module: null,
-							callType: 'get',
-							type: null,
-							id:null };
-
-		var gModular = null;
-		var finalObj = null;
-		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-		appLogger().info('Get call from ' + ip);
-
-		//this should be here if not we have a issue.
-		if(req.params.mod){
-	   		call.module = req.params.mod;
-	   	}else{
-	   		throw new Error("BAD MODULAR, MODULAR is a required.");
-	   		return;
-	   	}
-
-	   	//this should be here if not we have a issue.
-	   	if(req.params.type){
-	   		call.type = req.params.type;
-	   	}else{
-	   		throw new Error("BAD TYPE, TYPE is a required.");
-	   		return;
-	   	}
-
-	   	//this should be here if not we have a issue.
-	   	if(req.params.id){
-	   		call.id = req.params.id;
-	   	}else{
-	   		throw new Error("BAD ID, ID is a required, it cannot be found or is incorrect.");
-	   		return;
-	   	}
-
-	   	if(GLOBAL.getAppMode() !== 'development'){
-	   		   	//check for API KEY.
-			    if(req.query.apikey){
-			    	apikey = req.query.apikey;
-			    	appLogger().info('API Key = ' + apikey);
-			    }
-			   	if(!checkAPI(apikey)){
-			   		throw new Error("BAD API KEY, APIKEY is required parameter.");
-			   		return;
-			   	}
-	   	}
-
-	   	appLogger().info(JSON.stringify(call));
-	
-	   	gModular  = require(GLOBAL.CONTROLLERS  + call.module + 'Controller');
-	   	gModular.init(req, res);
-
-	   	finalObj = gModular.results();
-	   	if(finalObj && Array.isArray(finalObj)){
-	   		appLogger().info('Get call ' + call.module + ' complete..');
-	   		res.send(finalObj);
-	   	}else{
-	   		throw new Error("Issue with returned data.");
-	   	}
-	   	gModular.cleanUp();
+	//api defined.
+	app.get('/:mod/:type/:id', function(req, res){
+		request.getCallBack(req, res);
+	});
+	app.get('/:mod/:id', function(req, res){
+		request.getCB(req, res);
+	});
+	app.post('/:mod', function(req, res){
+		request.postCallBack(req, res);
 	});
 
-	app.get('/:mod/:id', function(req, res) {
-		//required parameters
-		var apikey = null; 
-		var call = {
-							module: null,
-							callType: 'get',
-							type: null,
-							id:null };
-
-		var gModular = null;
-		var finalObj = null;
-		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-		appLogger().info('Get call from ' + ip);
-
-		//this should be here if not we have a issue.
-		if(req.params.mod){
-	   		call.module = req.params.mod;
-	   	}else{
-	   		throw new Error("BAD MODULAR, MODULAR is a required.");
-	   		return;
-	   	}
-
-	   	//this should be here if not we have a issue.
-	   	if(req.params.id){
-	   		call.id = req.params.id;
-	   	}else{
-	   		throw new Error("BAD ID, ID is a required, it cannot be found or is incorrect.");
-	   		return;
-	   	}
-
-	   	if(GLOBAL.getAppMode() !== 'development'){
-	   		   	//check for API KEY.
-			    if(req.query.apikey){
-			    	apikey = req.query.apikey;
-			    	appLogger().info('API Key = ' + apikey);
-			    }
-			   	if(!checkAPI(apikey)){
-			   		throw new Error("BAD API KEY, APIKEY is required parameter.");
-			   		return;
-			   	}
-	   	}
-
-	   	appLogger().info(JSON.stringify(call));
-	
-	   	gModular  = require(GLOBAL.CONTROLLERS  + call.module + 'Controller');
-	   	gModular.init(req, res);
-
-	   	finalObj = gModular.results();
-	   	if(finalObj && Array.isArray(finalObj)){
-	   		appLogger().info('Get call ' + call.module + ' complete..');
-	   		res.send(finalObj);
-	   	}else{
-	   		throw new Error("Issue with returned data.");
-	   	}
-	   	gModular.cleanUp();
-	});
-
-	app.post('/:mod', function(req, res) {
-		//required parameters
-		var apikey = null; 
-		var call = {
-							module: null,
-							callType: 'post' };
-
-		var gModular = null;
-		var finalObj = null;
-		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-		appLogger().info('Post from ' + ip);
-
-		//this should be here if not we have a issue.
-		if(req.params.mod){
-	   		call.module = req.params.mod;
-	   	}else{
-	   		throw new Error("BAD MODULAR, MODULAR is a required.");
-	   		return;
-	   	}
-
-	   	if(GLOBAL.getAppMode() !== 'development'){
-	   		   	//check for API KEY.
-			    if(req.query.apikey){
-			    	apikey = req.query.apikey;
-			    	appLogger().info('API Key = ' + apikey);
-			    }
-			   	if(!checkAPI(apikey)){
-			   		throw new Error("BAD API KEY, APIKEY is required parameter.");
-			   		return;
-			   	}
-	   	}
-
-	   	appLogger().info(JSON.stringify(call));
-	
-	   	gModular  = require(GLOBAL.CONTROLLERS  + call.module + 'Controller');
-	   	gModular.init(req, res);
-
-	   	finalObj = gModular.results();
-	   	if(finalObj && Array.isArray(finalObj)){
-	   		appLogger().info('Get call ' + call.module + ' complete..');
-	   		res.send(finalObj);
-	   	}else{
-	   		throw new Error("Issue with returned data.");
-	   	}
-	   	gModular.cleanUp();
-	});
-
-//error handling...
+	//error handling...
 	app.use(function(err, req, res, next){
 	   res.send(500, err.toString());
 	  appLogger().error(err.stack);
---
+++ SideNoteWebServer/Lib/Request.js
@@ -0,0 +1,195 @@
+// Request.js
+var moduleName = 'Request';
+
+var validator = require(GLOBAL.LIB + 'Validator');
+var decrypter = require(GLOBAL.LIB + 'Decrypter');
+
+var timeStamp = function(){
+	var currentdate = new Date(); 
+	var timeStamped = currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+	return timeStamped;
+};
+
+module.exports = {
+	getCB: function(req, res){
+		//required parameters
+		var apikey = null; 
+		var call = {
+							module: null,
+							callType: 'get',
+							type: null,
+							id:null };
+
+		var gModular = null;
+		var finalObj = null;
+		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+		appLogger().info('Get call from ' + ip);
+
+		//this should be here if not we have a issue.
+		if(req.params.mod){
+	   		call.module = req.params.mod;
+	   	}else{
+	   		throw new Error("BAD MODULAR, MODULAR is a required.");
+	   		return;
+	   	}
+
+	   	//this should be here if not we have a issue.
+	   	if(req.params.id){
+	   		call.id = req.params.id;
+	   	}else{
+	   		throw new Error("BAD ID, ID is a required, it cannot be found or is incorrect.");
+	   		return;
+	   	}
+
+	   	if(GLOBAL.getAppMode() !== 'development'){
+	   		   	//check for API KEY.
+			    if(req.query.apikey){
+			    	apikey = req.query.apikey;
+			    	appLogger().info('API Key = ' + apikey);
+			    }
+			   	if(!checkAPI(apikey)){
+			   		throw new Error("BAD API KEY, APIKEY is required parameter.");
+			   		return;
+			   	}
+	   	}
+
+	   	appLogger().info(JSON.stringify(call));
+	
+	   	gModular  = require(GLOBAL.CONTROLLERS  + call.module + 'Controller');
+	   	gModular.init(req, res);
+
+	   	finalObj = gModular.results();
+	   	if(finalObj && Array.isArray(finalObj)){
+	   		appLogger().info('Get call ' + call.module + ' complete..');
+	   		finalObj.push({timeStamp: timeStamp()});
+	   		res.send(finalObj);
+	   	}else{
+	   		throw new Error("Issue with returned data.");
+	   	}
+	   	gModular.cleanUp();
+	},
+
+	postCallBack: function(req, res) {
+		//required parameters
+		var apikey = null; 
+		var call = {
+							module: null,
+							callType: 'post' };
+
+		var gModular = null;
+		var finalObj = null;
+		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+		appLogger().info('Post from ' + ip);
+
+		//this should be here if not we have a issue.
+		if(req.params.mod){
+	   		call.module = req.params.mod;
+	   	}else{
+	   		throw new Error("BAD MODULAR, MODULAR is a required.");
+	   		return;
+	   	}
+
+	   	if(GLOBAL.getAppMode() !== 'development'){
+	   		   	//check for API KEY.
+			    if(req.query.apikey){
+			    	apikey = req.query.apikey;
+			    	appLogger().info('API Key = ' + apikey);
+			    }
+			   	if(!checkAPI(apikey)){
+			   		throw new Error("BAD API KEY, APIKEY is required parameter.");
+			   		return;
+			   	}
+	   	}
+
+	   	appLogger().info(JSON.stringify(call));
+	
+	   	gModular  = require(GLOBAL.CONTROLLERS  + call.module + 'Controller');
+	   	gModular.init(req, res);
+
+	   	finalObj = gModular.results();
+	   	if(finalObj && Array.isArray(finalObj)){
+	   		appLogger().info('Get call ' + call.module + ' complete..');
+	   		finalObj.push({timeStamp: timeStamp()});
+	   		res.send(finalObj);
+	   	}else{
+	   		throw new Error("Issue with returned data.");
+	   	}
+	   	gModular.cleanUp();
+	},
+
+	getCallBack: function(req, res) {
+		//required parameters
+		var apikey = null; 
+		var call = {
+			module: null,
+			callType: 'get',
+			type: null,
+			id:null };
+
+		var gModular = null;
+		var finalObj = null;
+		var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+		appLogger().info('Get call from ' + ip);
+
+		//this should be here if not we have a issue.
+		if(req.params.mod){
+	   		call.module = req.params.mod;
+	   	}else{
+	   		throw new Error("BAD MODULAR, MODULAR is a required.");
+	   		return;
+	   	}
+
+	   	//this should be here if not we have a issue.
+	   	if(req.params.type){
+	   		call.type = req.params.type;
+	   	}else{
+	   		throw new Error("BAD TYPE, TYPE is a required.");
+	   		return;
+	   	}
+
+	   	//this should be here if not we have a issue.
+	   	if(req.params.id){
+	   		call.id = req.params.id;
+	   	}else{
+	   		throw new Error("BAD ID, ID is a required, it cannot be found or is incorrect.");
+	   		return;
+	   	}
+
+	   	if(GLOBAL.getAppMode() !== 'development'){
+	   		   	//check for API KEY.
+			    if(req.query.apikey){
+			    	apikey = req.query.apikey;
+			    	appLogger().info('API Key = ' + apikey);
+			    }
+			   	if(!checkAPI(apikey)){
+			   		throw new Error("BAD API KEY, APIKEY is required parameter.");
+			   		return;
+			   	}
+	   	}
+
+	   	appLogger().info(JSON.stringify(call));
+	
+	   	gModular  = require(GLOBAL.CONTROLLERS  + call.module + 'Controller');
+	   	gModular.init(req, res);
+
+	   	finalObj = gModular.results();
+	   	if(finalObj && Array.isArray(finalObj)){
+	   		appLogger().info('Get call ' + call.module + ' complete..');
+	   		finalObj.push({timeStamp: timeStamp()});
+	   		res.send(finalObj);
+	   	}else{
+	   		throw new Error("Issue with returned data.");
+	   	}
+	   	gModular.cleanUp();
+	},
+
+}
