0c61c7c76477f98c53476c2d31679e8211bfd84b
Jerum Hubbert <jerumhubbert@Jerums-MacBook-Pro-2.local>
2014-08-26 16:44:44 -0700

    stated on the layout of the main login page, still trying to create a good design for the JS

--- SideNoteWebServer/www/js/bootstrap.js
+++ SideNoteWebServer/www/js/bootstrap.js
@@ -1,1 +1,8 @@
-console.log("this bootstrap my app and setup all framwork needed to create this app");
+//clears my content div
+$('#content').empty();
+
+//give the article a id so we can hook up the css to it.
+$('article.main').attr("id", "loginPage");
+
+//add stuff to content, this will be broken out into a templ design
+$('#content').append('<p>This will be the html for the login page!</p>');
