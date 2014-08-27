a74eb82428c55f49f5c4d1076e11abb59f3a3c46
Jerum Hubbert <hubbertj@gmail.com>
2014-08-26 18:54:53 -0700

    updated

--- SideNoteWebServer/www/js/bootstrap.js
+++ SideNoteWebServer/www/js/bootstrap.js
@@ -2,7 +2,7 @@
 $('#content').empty();
 
 //give the article a id so we can hook up the css to it.
-$('article.main').attr("id", "loginPage");
+$('article.main').     attr("id", "loginPage");
 
 //add stuff to content, this will be broken out into a templ design
 $('#content').append('<p>This will be the html for the login page!</p>');
