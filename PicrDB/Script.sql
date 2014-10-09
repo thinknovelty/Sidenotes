1efbaf38f717da71e3aa1a8f654335f099801527
Bobby DeArmond <thinknovelty@gmail.com>
2014-10-09 15:22:51 -0700

    Tables are now account-centric.
    Merged 'user_account' and 'user_credentials' into 'account'. All tables now reference 'account._id' as the primary _id.

--- PicrDB/Script.sql
+++ PicrDB/Script.sql
@@ -8,31 +8,40 @@ CREATE DATABASE picr_development;
 
 USE picr_development;
 
--- Stores all pictures
--- Pictures will be stored in multiple directories: original, scaled
-CREATE TABLE IF NOT EXISTS picture (
-	_id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
-	name		VARCHAR(255),
-    created_on	DATETIME NOT NULL,
-    PRIMARY KEY(_id)
+-- Stores user credentials for logging in.
+CREATE TABLE IF NOT EXISTS account (
+    _id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    email		VARCHAR(254) NOT NULL,
+    password		VARCHAR(254) NOT NULL,
+    salt		VARCHAR(32) NOT NULL,
+    locked		BOOL NOT NULL,
+    closed		BOOL NOT NULL,
+    verified		BOOL NOT NULL,
+    locked_on		DATETIME,
+    closed_on		DATETIME,
+    verified_on		DATETIME,
+    created_on		DATETIME NOT NULL,
+    PRIMARY KEY(_id),
+    CONSTRAINT u_user_credentials UNIQUE (email)
 ) ENGINE=InnoDB;
 
 -- Primary user table for basic user information
-CREATE TABLE IF NOT EXISTS user (
-	_id				INT UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS user_info (
+    _id			INT UNSIGNED NOT NULL,
     first_name		VARCHAR(35) NOT NULL,
     last_name		VARCHAR(35) NOT NULL,
-    sex				CHAR(1) NOT NULL,
+    sex			CHAR(1) NOT NULL,
     birthday		DATE NOT NULL,
-    PRIMARY KEY(_id)
+    PRIMARY KEY(_id),
+    FOREIGN KEY(_id) REFERENCES account(_id)
 ) ENGINE=InnoDB;
 
 -- Role types will be defined for each employee roles such as 'administrator'
 CREATE TABLE IF NOT EXISTS role_type (
-	_id					SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name				VARCHAR(35) NOT NULL,
-    description			VARCHAR(35) NOT NULL,
-    created_on			DATETIME NOT NULL,
+    _id			SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name		VARCHAR(35) NOT NULL,
+    description		VARCHAR(35) NOT NULL,
+    created_on		DATETIME NOT NULL,
     PRIMARY KEY(_id),
     CONSTRAINT u_role UNIQUE(name)
 ) ENGINE=InnoDB;
@@ -40,25 +49,14 @@ CREATE TABLE IF NOT EXISTS role_type (
 -- Roles will only be defined for employees.
 -- Regular users will not have a record in this table.
 CREATE TABLE IF NOT EXISTS user_role (
-	_id				INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id			INT UNSIGNED NOT NULL,
+    _id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    account_id		INT UNSIGNED NOT NULL,
     role_type_id	SMALLINT UNSIGNED NOT NULL,
     created_on		DATETIME NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(user_id) REFERENCES user(_id),
+    FOREIGN KEY(account_id) REFERENCES account(_id),
     FOREIGN KEY(role_type_id) REFERENCES role_type(_id),
-    CONSTRAINT u_user_role UNIQUE(user_id, role_type_id)
-) ENGINE=InnoDB;
-
--- Stores user credentials for logging in.
-CREATE TABLE IF NOT EXISTS user_credentials (
-	_id				INT UNSIGNED NOT NULL,
-    email			VARCHAR(254) NOT NULL,
-    password		VARCHAR(254) NOT NULL,
-    salt			VARCHAR(32) NOT NULL,
-    PRIMARY KEY(_id),
-    FOREIGN KEY(_id) REFERENCES user(_id),
-    CONSTRAINT u_user_credentials UNIQUE (email)
+    CONSTRAINT u_user_role UNIQUE(account_id, role_type_id)
 ) ENGINE=InnoDB;
 
 -- This is used purely for verifying initial registration.
@@ -71,27 +69,13 @@ CREATE TABLE IF NOT EXISTS user_credentials (
 -- an e-mail will be sent out that the user must register again.
 -- Upon registration, this record will be deleted.
 CREATE TABLE IF NOT EXISTS user_verification (
-	_id				INT UNSIGNED NOT NULL,
-    code			VARCHAR(32) NOT NULL,
+    _id			INT UNSIGNED NOT NULL,
+    code		VARCHAR(32) NOT NULL,
     verified		BOOL NOT NULL,
     created_on		DATETIME NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(_id) REFERENCES user(_id),
+    FOREIGN KEY(_id) REFERENCES account(_id),
     CONSTRAINT u_user_verification UNIQUE (_id)
-) ENGINE=InnoDB;
-
--- Stores user account state information
-CREATE TABLE IF NOT EXISTS user_account (
-	_id			INT UNSIGNED NOT NULL,
-    locked		BOOL NOT NULL,
-    closed		BOOL NOT NULL,
-    verified	BOOL NOT NULL,
-    locked_on	DATETIME,
-    closed_on	DATETIME,
-    verified_on	DATETIME,
-    created_on	DATETIME NOT NULL,
-    PRIMARY KEY(_id),
-    FOREIGN KEY(_id) REFERENCES user(_id)
 ) ENGINE=InnoDB;
 
 -- Stores all login attempts, pass or fail.
@@ -100,12 +84,12 @@ CREATE TABLE IF NOT EXISTS user_account (
 -- time due to too many failed login attempts.
 -- Should prompt a user to reset their password.
 CREATE TABLE IF NOT EXISTS user_login_attempts (
-	_id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id	INT UNSIGNED NOT NULL,
+    _id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    account_id	INT UNSIGNED NOT NULL,
     success	BOOL NOT NULL,
     created_on	DATETIME NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(user_id) REFERENCES user(_id)
+    FOREIGN KEY(account_id) REFERENCES account(_id)
 ) ENGINE=InnoDB;
 
 -- This table tracks user sessions.
@@ -113,40 +97,40 @@ CREATE TABLE IF NOT EXISTS user_login_attempts (
 -- based on a user_id. Later this will track
 -- user sessions based on the device and user_id
 CREATE TABLE IF NOT EXISTS user_session (
-	_id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id		INT UNSIGNED NOT NULL,
+    _id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    account_id		INT UNSIGNED NOT NULL,
     uuid		VARCHAR(36) NOT NULL,
     created_on	DATETIME NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(user_id) REFERENCES user(_id),
+    FOREIGN KEY(account_id) REFERENCES account(_id),
     CONSTRAINT u_user_session UNIQUE (uuid)
 ) ENGINE=InnoDB;
 
 -- Different user connections such as friends, brother, sister, etc...
 CREATE TABLE IF NOT EXISTS connection_type (
-	_id			SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    _id			SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     name		VARCHAR(35) NOT NULL,
     description	VARCHAR(35) NOT NULL,
-	PRIMARY KEY(_id),
+    PRIMARY KEY(_id),
     CONSTRAINT u_connection_type UNIQUE(name)
 ) ENGINE=InnoDB;
 
 -- Links the user to their connection and specifies the connection type
 CREATE TABLE IF NOT EXISTS user_connection (
-	_id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id		INT UNSIGNED NOT NULL,
+    _id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    account_id		INT UNSIGNED NOT NULL,
     conn_id		INT UNSIGNED NOT NULL,
     created_on	DATETIME NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(user_id) REFERENCES user(_id),
-    FOREIGN KEY(conn_id) REFERENCES user(_id),
-    CONSTRAINT u_user_conn UNIQUE (user_id, conn_id)
+    FOREIGN KEY(account_id) REFERENCES account(_id),
+    FOREIGN KEY(conn_id) REFERENCES account(_id),
+    CONSTRAINT u_user_conn UNIQUE (account_id, conn_id)
 ) ENGINE=InnoDB;
 
 -- A user can have multiple connection types for a single connection
 -- For example: A connection can be both a friend and a coworker
 CREATE TABLE IF NOT EXISTS user_connection_type (
-	_id				INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    _id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_conn_id	INT UNSIGNED NOT NULL,
     conn_type		SMALLINT UNSIGNED NOT NULL,
     PRIMARY KEY(_id),
@@ -157,34 +141,43 @@ CREATE TABLE IF NOT EXISTS user_connection_type (
 -- Stores the history of picks a user has participated in and
 -- what they picked.
 CREATE TABLE IF NOT EXISTS user_pick_history (
-	_id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id		INT UNSIGNED NOT NULL,
-    pick_id		INT UNSIGNED NOT NULL,
+    _id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    account_id	INT UNSIGNED NOT NULL,
+    pick_id	INT UNSIGNED NOT NULL,
     picture_id	INT UNSIGNED NOT NULL,
     created_on	DATETIME NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(user_id) REFERENCES user(_id),
+    FOREIGN KEY(account_id) REFERENCES account(_id),
     FOREIGN KEY(picture_id) REFERENCES picture(_id),
-    CONSTRAINT u_user_pick_history UNIQUE(user_id, pick_id)
+    CONSTRAINT u_user_pick_history UNIQUE(account_id, pick_id)
+) ENGINE=InnoDB;
+
+-- Stores all pictures
+-- Pictures will be stored in multiple directories: original, scaled
+CREATE TABLE IF NOT EXISTS picture (
+    _id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name	VARCHAR(255),
+    created_on	DATETIME NOT NULL,
+    PRIMARY KEY(_id)
 ) ENGINE=InnoDB;
 
 -- The pick created by a user
 CREATE TABLE IF NOT EXISTS pick (
-	_id				INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    user_id			INT UNSIGNED NOT NULL,
-	question		VARCHAR(35),
+    _id			INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    account_id		INT UNSIGNED NOT NULL,
+    question		VARCHAR(35),
     picture_1_id	INT UNSIGNED NOT NULL,
     picture_2_id	INT UNSIGNED NOT NULL,
     created_on		DATETIME NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(user_id) REFERENCES user(_id),
+    FOREIGN KEY(account_id) REFERENCES account(_id),
     FOREIGN KEY(picture_1_id) REFERENCES picture(_id),
     FOREIGN KEY(picture_2_id) REFERENCES picture(_id)
 ) ENGINE=InnoDB;
 
 -- The share type is who the pick will be shared to.
 CREATE TABLE IF NOT EXISTS pick_share_type (
-	_id			SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    _id			SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     name		VARCHAR(35),
     description	VARCHAR(35),
     created_on	DATETIME NOT NULL,
@@ -194,14 +187,14 @@ CREATE TABLE IF NOT EXISTS pick_share_type (
 
 -- This is the state of the pick.
 CREATE TABLE IF NOT EXISTS pick_state (
-	_id				INT UNSIGNED NOT NULL,
-    closed			BOOL NOT NULL,
-    pick_1			MEDIUMINT UNSIGNED NOT NULL,
-    pick_2			MEDIUMINT UNSIGNED NOT NULL,
+    _id			INT UNSIGNED NOT NULL,
+    closed		BOOL NOT NULL,
+    pick_1		MEDIUMINT UNSIGNED NOT NULL,
+    pick_2		MEDIUMINT UNSIGNED NOT NULL,
     close_on_pick	MEDIUMINT UNSIGNED,
     close_on_time	DATETIME,
     share_type_id	SMALLINT UNSIGNED NOT NULL,
-    open_on			DATETIME NOT NULL,
+    open_on		DATETIME NOT NULL,
     close_on		DATETIME,
     created_on		DATETIME NOT NULL,
     PRIMARY KEY(_id),
@@ -211,19 +204,19 @@ CREATE TABLE IF NOT EXISTS pick_state (
 
 -- This stores the list of users following a particular pick.
 CREATE TABLE IF NOT EXISTS pick_follow (
-	_id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    _id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id	INT UNSIGNED NOT NULL,
     pick_id	INT UNSIGNED NOT NULL,
     PRIMARY KEY(_id),
-    FOREIGN KEY(user_id) REFERENCES user(_id),
+    FOREIGN KEY(account_id) REFERENCES account(_id),
     FOREIGN KEY(pick_id) REFERENCES pick(_id),
-    CONSTRAINT u_pick_follow UNIQUE(user_id, pick_id)
+    CONSTRAINT u_pick_follow UNIQUE(account_id, pick_id)
 ) ENGINE=InnoDB;
 
 -- These tags will act as "hashtags" for the application and
 -- can and will correspond with real hashtags.
 CREATE TABLE IF NOT EXISTS tag (
-	_id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    _id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
     name	VARCHAR(140), -- HASHTAG MAX SIZE
     PRIMARY KEY(_id),
     CONSTRAINT u_tag UNIQUE(name)
@@ -231,7 +224,7 @@ CREATE TABLE IF NOT EXISTS tag (
 
 -- This will connect picks and tags
 CREATE TABLE IF NOT EXISTS pick_tag (
-	_id	INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    _id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
     tag_id	INT UNSIGNED NOT NULL,
     pick_id	INT UNSIGNED NOT NULL,
     PRIMARY KEY(_id),
@@ -242,10 +235,10 @@ CREATE TABLE IF NOT EXISTS pick_tag (
 
 -- Settings used for the backend application
 CREATE TABLE IF NOT EXISTS settings (
-	_id				INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    name			VARCHAR(35) NOT NULL,
-    value			VARCHAR(35) NOT NULL,
-    active			BOOL,
-    created_on		DATETIME NOT NULL,
+    _id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name	VARCHAR(35) NOT NULL,
+    value	VARCHAR(35) NOT NULL,
+    active	BOOL,
+    created_on	DATETIME NOT NULL,
     PRIMARY KEY(_id)
 ) ENGINE=InnoDB;
